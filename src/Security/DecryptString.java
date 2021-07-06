cb8a25f9f72949c9aa3a148a193e1b91de85662e
#_507 <30375404+NguyenTuong95@users.noreply.github.com>
2021-07-06 18:54:30 +0700

    update

---
+++ src/Security/DecryptString.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Security;
+
+/**
+ *
+ * @author Hung
+ */
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+ 
+public class DecryptString {
+  private static final String SECRET_KEY = "my_super_secret_key_ho_ho_ho";
+  private static final String SALT = "ssshhhhhhhhhhh!!!!"; 
+  public static String decrypt(String strToDecrypt) {
+    try {
+      byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      IvParameterSpec ivspec = new IvParameterSpec(iv);
+      // Băm mật khẩu kết hợp với muối để bảo vệ mật khẩu
+      SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+      // 65536 -- số bước lặp, chiều dài của khoá 256
+      KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALT.getBytes(), 65536, 256);
+      SecretKey tmp = factory.generateSecret(spec);
+      SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+ 
+      Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+      cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
+      return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+    } catch (Exception e) {
+      System.out.println("Error while decrypting: " + e.toString());
+    }
+    return null;
+  }
+}
---
+++ src/Security/EncryptString.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Security;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+ 
+public class EncryptString {
+  private static final String SECRET_KEY = "my_super_secret_key_ho_ho_ho";
+  private static final String SALT = "ssshhhhhhhhhhh!!!!"; 
+  public static String encrypt(String strToEncrypt) {
+    try {
+      byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      IvParameterSpec ivspec = new IvParameterSpec(iv);
+      // Sử dụng thuật toán PBKDF2 với hàm băm SHA256 để bảo vệ SECRET_KEY
+      SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+      //65536 - số bước lặp, 256 -- độ dài khoá
+      KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALT.getBytes(), 65536, 256);
+      SecretKey tmp = factory.generateSecret(spec);
+      SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES"); 
+ 
+      Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+      cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+      return Base64.getEncoder()
+          .encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
+    } catch (Exception e) {
+      System.out.println("Error while encrypting: " + e.toString());
+    }
+    return null;
+  }
+}

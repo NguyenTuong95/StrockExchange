6d58aafd6ed46109a8fd619b82184587e12c10ee
#_507 <30375404+NguyenTuong95@users.noreply.github.com>
2021-07-06 10:05:56 +0700

    Update

--- src/Model/Stock.java
+++ src/Model/Stock.java
@@ -8,19 +8,21 @@ public class Stock {
     private float lastPrice;
     private int minPrice;
     private int maxPrice;
+    private int totalAmount;
     private static int cnt = 0;
 
     public Stock(){
         this.stockID = this.companyID = cnt++;
     }
 
-    public Stock(int stockID, int companyID, String code, float lastPrice, int minPrice, int maxPrice){
+    public Stock(int stockID, int companyID, String code, float lastPrice, int minPrice, int maxPrice, int totalAmount){
         this.stockID = stockID;
         this.companyID = companyID;
         this.code = code;
         this.lastPrice = lastPrice;
         this.minPrice = minPrice;
         this.maxPrice = maxPrice;
+        this.totalAmount = totalAmount;
     }
 
     public int getStockID() {
@@ -71,11 +73,18 @@ public class Stock {
         this.maxPrice = maxPrice;
     }
 
+    public int getTotalAmount() {
+		return this.totalAmount;
+	}
+
+	public void setTotalAmount(int totalAmount) {
+		this.totalAmount = totalAmount;
+	}
 
     @Override
     public String toString() { 
         return " --[STOCK] ID: " + this.stockID + " code: " + this.code+ " lastPrice: " + this.lastPrice 
-        + "minPrice: " + this.minPrice + "maxPrice: " + this.maxPrice + " --";
+        + "minPrice: " + this.minPrice + "maxPrice: " + this.maxPrice +  "totalAmount:" + this.totalAmount + "--";
     }
 
 }
